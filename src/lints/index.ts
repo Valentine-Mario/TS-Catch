7c1d4d6c7169da03175c124fe52a89f9780b88fb
valentine-mario <valentine13400@gmail.com>
2021-08-09 09:47:14 +0100

    renamed files

--- src/lints/index.ts
+++ src/lints/index.ts
@@ -1,4 +1,4 @@
 import { useLetInFuncScope, useLetInIIFScope } from "./use_let_in_func_scope";
-import { useBooleanInFuncType } from "./use_boolean_in_func_type";
+import { useBooleanInFuncType } from "./use_primitive_in_func_type";
 
 export { useLetInFuncScope, useBooleanInFuncType, useLetInIIFScope };
