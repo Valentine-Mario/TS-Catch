db8967f374b111cbc52587633f4e30c96daf058a
Valentine-Mario <valentine13400@gmail.com>
2022-01-20 19:35:49 +0100

    added class definition

--- src/get_definitions/get_class_methods.ts
+++ src/get_definitions/get_class_methods.ts
@@ -1,9 +1,15 @@
-import { Statement, ts } from "ts-morph";
+import { ClassDeclaration, Statement, ts } from "ts-morph";
+let all_classes: ClassDeclaration[] = [];
 
 const get_class_methods = (
-  input: Statement<ts.Statement>
-): Statement<ts.Statement>[] => {
-  return;
+  input: Statement<ts.Statement>[]
+): ClassDeclaration[] => {
+  for (const classItem of input) {
+    if (classItem instanceof ClassDeclaration) {
+      all_classes.push(classItem);
+    }
+  }
+  return all_classes;
 };
 
 export default get_class_methods;
